ccede473466611951472843e5b73efcd0211b7c8
Julian Mendez <julian.mendez@tu-dresden.de>
2015-08-13 20:54:52 +0200

    Add Hello World program

---
+++ wikihypergraph/src/main/java/de/tudresden/inf/lat/wikihypergraph/main/Main.java
@@ -0,0 +1,16 @@
+
+package de.tudresden.inf.lat.wikihypergraph.main;
+
+
+/**
+ * @author Julian Mendez
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
+
+
---
+++ wikihypergraph/src/main/java/de/tudresden/inf/lat/wikihypergraph/main/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * @author Julian Mendez
+ */
+
+/**
+ * Provides interfaces and classes for the main package.
+ */
+package de.tudresden.inf.lat.wikihypergraph.main;
+
