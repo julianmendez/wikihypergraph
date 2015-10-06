ad9554e02761482be8658626fe519879bc774673
Julian Mendez <julian.mendez@tu-dresden.de>
2015-10-06 17:12:05 +0200

    Refactor entity collector

--- wikihypergraph/src/main/java/de/tudresden/inf/lat/wikihypergraph/module/ModuleExtractionMwRevisionProcessor.java
+++ wikihypergraph/src/main/java/de/tudresden/inf/lat/wikihypergraph/module/ModuleExtractionMwRevisionProcessor.java
@@ -5,37 +5,16 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
-import org.wikidata.wdtk.datamodel.helpers.Datamodel;
-import org.wikidata.wdtk.datamodel.interfaces.Claim;
-import org.wikidata.wdtk.datamodel.interfaces.DatatypeIdValue;
-import org.wikidata.wdtk.datamodel.interfaces.EntityIdValue;
-import org.wikidata.wdtk.datamodel.interfaces.GlobeCoordinatesValue;
-import org.wikidata.wdtk.datamodel.interfaces.MonolingualTextValue;
-import org.wikidata.wdtk.datamodel.interfaces.NoValueSnak;
-import org.wikidata.wdtk.datamodel.interfaces.QuantityValue;
-import org.wikidata.wdtk.datamodel.interfaces.Snak;
-import org.wikidata.wdtk.datamodel.interfaces.SnakVisitor;
-import org.wikidata.wdtk.datamodel.interfaces.SomeValueSnak;
-import org.wikidata.wdtk.datamodel.interfaces.Statement;
-import org.wikidata.wdtk.datamodel.interfaces.StatementGroup;
-import org.wikidata.wdtk.datamodel.interfaces.StringValue;
-import org.wikidata.wdtk.datamodel.interfaces.TimeValue;
-import org.wikidata.wdtk.datamodel.interfaces.ValueSnak;
-import org.wikidata.wdtk.datamodel.interfaces.ValueVisitor;
-import org.wikidata.wdtk.datamodel.json.jackson.JacksonDatatypeId;
-import org.wikidata.wdtk.datamodel.json.jackson.JacksonTermedStatementDocument;
 import org.wikidata.wdtk.dumpfiles.MwRevision;
 import org.wikidata.wdtk.dumpfiles.MwRevisionProcessor;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * An object of this class extracts a 'module'.
@@ -44,8 +23,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  *
  */
 public class ModuleExtractionMwRevisionProcessor implements MwRevisionProcessor {
-
-	public static final String EXPECTED_FORMAT = "application/json";
 
 	private final Set<String> module = new TreeSet<String>();
 	private final Set<String> visited = new TreeSet<String>();
@@ -96,68 +73,6 @@ public class ModuleExtractionMwRevisionProcessor implements MwRevisionProcessor 
 
 	}
 
-	class EntitySnakVisitor implements SnakVisitor<Set<String>> {
-
-		@Override
-		public Set<String> visit(ValueSnak snak) {
-			ValueSnakVisitor valueVisitor = new ValueSnakVisitor();
-			Set<String> ret = new TreeSet<String>();
-			ret.addAll(snak.getValue().accept(valueVisitor));
-			ret.add(snak.getPropertyId().getId());
-			return ret;
-		}
-
-		@Override
-		public Set<String> visit(SomeValueSnak snak) {
-			return Collections.singleton(snak.getPropertyId().getId());
-		}
-
-		@Override
-		public Set<String> visit(NoValueSnak snak) {
-			return Collections.singleton(snak.getPropertyId().getId());
-		}
-
-	}
-
-	class ValueSnakVisitor implements ValueVisitor<Set<String>> {
-
-		@Override
-		public Set<String> visit(DatatypeIdValue value) {
-			return Collections.emptySet();
-		}
-
-		@Override
-		public Set<String> visit(EntityIdValue value) {
-			return Collections.singleton(value.getId());
-		}
-
-		@Override
-		public Set<String> visit(GlobeCoordinatesValue value) {
-			return Collections.emptySet();
-		}
-
-		@Override
-		public Set<String> visit(MonolingualTextValue value) {
-			return Collections.emptySet();
-		}
-
-		@Override
-		public Set<String> visit(QuantityValue value) {
-			return Collections.emptySet();
-		}
-
-		@Override
-		public Set<String> visit(StringValue value) {
-			return Collections.emptySet();
-		}
-
-		@Override
-		public Set<String> visit(TimeValue value) {
-			return Collections.emptySet();
-		}
-
-	}
-
 	public ModuleExtractionMwRevisionProcessor(Collection<String> items, Writer writer) {
 		this.output = new BufferedWriter(writer);
 		this.items.addAll(items);
@@ -171,47 +86,11 @@ public class ModuleExtractionMwRevisionProcessor implements MwRevisionProcessor 
 		this.toVisit.addAll(initialSet);
 	}
 
-	Set<String> collectEntities(MwRevision mwRevision) throws JsonProcessingException, IOException {
-		Set<String> ret = new TreeSet<String>();
-		List<StatementGroup> statementGroups = getStatementGroups(mwRevision);
-		for (StatementGroup statementGroup : statementGroups) {
-			ret.addAll(collectEntities(statementGroup));
-		}
-		return ret;
-	}
-
-	Set<String> collectEntities(StatementGroup statementGroup) {
-		Set<String> ret = new TreeSet<String>();
-		List<Statement> statements = statementGroup.getStatements();
-		for (Statement statement : statements) {
-			ret.addAll(collectEntities(statement));
-		}
-		return ret;
-	}
-
-	Set<String> collectEntities(Statement statement) {
-		Set<String> ret = new TreeSet<String>();
-		Claim claim = statement.getClaim();
-
-		if (claim.getSubject() != null) {
-			ret.add(claim.getSubject().getId());
-		}
-
-		Snak snak = claim.getMainSnak();
-
-		ret.add(snak.getPropertyId().getId());
-
-		EntitySnakVisitor entityVisitor = new EntitySnakVisitor();
-		ret.addAll(snak.accept(entityVisitor));
-
-		return ret;
-	}
-
 	public void visit(MwRevision mwRevision) throws JsonProcessingException, IOException {
 		String origPage = mwRevision.getTitle();
 		this.visited.add(origPage);
 
-		Set<String> pages = collectEntities(mwRevision);
+		Set<String> pages = (new EntityCollector()).collectEntities(mwRevision);
 		for (String page : pages) {
 			if (!this.visited.contains(page)) {
 				this.toVisit.add(page);
@@ -247,7 +126,7 @@ public class ModuleExtractionMwRevisionProcessor implements MwRevisionProcessor 
 		try {
 			if (this.toVisit.contains(title)) {
 				this.output.write(title);
-				Set<String> entities = collectEntities(mwRevision);
+				Set<String> entities = (new EntityCollector()).collectEntities(mwRevision);
 				this.output.write("\t" + entities.toString());
 				this.output.newLine();
 				this.output.flush();
@@ -269,24 +148,6 @@ public class ModuleExtractionMwRevisionProcessor implements MwRevisionProcessor 
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-	}
-
-	List<StatementGroup> getStatementGroups(MwRevision mwRevision) throws JsonProcessingException, IOException {
-		List<StatementGroup> ret = new ArrayList<StatementGroup>();
-		String format = mwRevision.getFormat();
-		String model = mwRevision.getModel();
-
-		if (format.equals(EXPECTED_FORMAT)
-				&& (model.equals(JacksonDatatypeId.JSON_DT_ITEM) || model.equals(JacksonDatatypeId.JSON_DT_PROPERTY))) {
-
-			ObjectMapper mapper = new ObjectMapper();
-			String text = mwRevision.getText();
-			JacksonTermedStatementDocument document = mapper.readValue(text, JacksonTermedStatementDocument.class);
-			document.setSiteIri(Datamodel.SITE_WIKIDATA);
-			ret.addAll(document.getStatementGroups());
-
-		}
-		return ret;
 	}
 
 }
