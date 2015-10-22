4149d6dcc77c82f73d5e19adb25a7e9ccd365ec4
Julian Mendez <julian.mendez@tu-dresden.de>
2015-10-22 13:22:37 +0200

    Add names of types and relations

---
+++ wikihypergraph/src/main/java/de/tudresden/inf/lat/wikihypergraph/selector/TypeAndRelationName.java
@@ -0,0 +1,15 @@
+package de.tudresden.inf.lat.wikihypergraph.selector;
+
+public interface TypeAndRelationName {
+
+	String TYPE_STATEMENT = "statement";
+	String TYPE_REFERENCE = "reference";
+	String TYPE_PAIR_VALUE = "pair-value";
+
+	String RELATION_HAS_TYPE = "has_type";
+	String RELATION_HAS_REFERENCE = " has_reference";
+	String RELATION_HAS_PAIR_VALUE = "has_pair_value";
+	String RELATION_HAS_PROPERTY = "has_property";
+	String RELATION_HAS_VALUE = "has_value";
+
+}
